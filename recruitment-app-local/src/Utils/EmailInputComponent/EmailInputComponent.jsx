864d55249921da7350e73c47a94fa3b9dddf0e72
00007518 <00007518@uca.edu.sv>
2023-07-21 07:38:07 -0600

    Finish::sparkles:Make responsive the view to fill form

--- recruitment-app-local/src/Utils/EmailInputComponent/EmailInputComponent.jsx
+++ recruitment-app-local/src/Utils/EmailInputComponent/EmailInputComponent.jsx
@@ -6,8 +6,10 @@ function EmailInputComponent(props){
     return(
         <div className='emailInputComponent'>
             <section className='emailsTitleSection'>
-                <p>{props.title}</p>
-                {<FiAlertCircle className='alertIconInput'/>}
+                <article className='titleInputEmail'>
+                    <p>{props.title}</p>
+                    {<FiAlertCircle className='alertIconInput'/>}
+                </article>
                 <input type="text" placeholder={props.placeholder}/>
                 <button className='btnAddMail'>Añadir</button>
             </section>
