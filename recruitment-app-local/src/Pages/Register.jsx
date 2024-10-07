bac3bbb9c080aca95b2e503519880dbc86d78dba
00007518 <00007518@uca.edu.sv>
2024-10-07 14:39:47 -0600

    delete all

--- recruitment-app-local/src/Pages/Register.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import RegisterComponent from "../Components/RegisterComponent/RegisterComponent";
-
-function Register() {
-  return <RegisterComponent/>;
-}
-
-export default Register;
