d9be3fbefdd8909e31e853488d79544d585b9d36
00007518 <00007518@uca.edu.sv>
2024-10-07 14:42:26 -0600

    delete all

--- recruitment-app-local/src/Pages/FormInscriptions.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import FormInstructors from "../Components/FormInstructors/FormInstructors";
-
-function FormInscriptions() {
-    return <FormInstructors/>
-}
-
-export default FormInscriptions;
